ac8946d922de9e8cb97557ccfe04229750e4c592
Utsob Roy <uroybd@gmail.com>
2015-06-29 11:19:48 +0600

    Caesar_Cipher.clj final

--- Algorithms/Warmup/Caesar_Cipher.clj
+++ Algorithms/Warmup/Caesar_Cipher.clj
@@ -5,16 +5,27 @@
 (def cipherkey (read-string (read-line)))
 
 (defn cipher [i key]
-  (let [inum (int (.charAt i 0))]
-    (cond (and (>= inum 65) (<= inum 90))
-          (if (> (+ inum key) 90)
-            (char (- (+ inum key) 26))
-            (char (+ inum key)))
-          (and (>= inum 96) (<= inum 122))
-          (if (> (+ inum key) 122)
-            (char (- (+ inum key) 26))
-            (char (+ inum key)))
-          :else (char inum))))
+  (let [indxval (int (.charAt i 0))]
+    (cond
+      (and (>= indxval 65) (<= indxval 90))
+      (let [trueindx (- indxval 64)
+            truestep (rem (+ key (- indxval 64)) 26)]
+        (cond
+          (> truestep 26)
+          (char (+ 64 (rem truestep 26)))
+          (= truestep 0)
+          (char (+ 64 26))
+          :else (char (+ 64 truestep))))
+      (and (>= indxval 97) (<= indxval 122))
+      (let [trueindx (- indxval 96)
+            truestep (rem (+ key (- indxval 96)) 26)]
+        (cond
+          (> truestep 26)
+          (char (+ 96 (rem truestep 26)))
+          (= truestep 0)
+          (char (+ 96 26))
+          :else (char (+ 96 truestep))))
+      :else (char indxval))))
 
 (swap! strvec (fn [n] (map #(cipher % cipherkey) n)))
 (println (clojure.string/join @strvec))
