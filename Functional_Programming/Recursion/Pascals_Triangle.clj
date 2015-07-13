309b486093d8b087d5444818f491486657d15510
Utsob Roy <uroybd@gmail.com>
2015-07-13 12:46:38 +0600

    Pascals_Triangle.clj

--- Functional_Programming/Recursion/Pascals_Triangle.clj
+++ Functional_Programming/Recursion/Pascals_Triangle.clj
@@ -1,24 +1,16 @@
 (defn factorial [n]
-  (reduce * (map bigint (take n (range 1 (inc n))))))
+  (reduce * (take n (range 1 (inc n)))))
 
 (defn triangle [limit]
-  (loop [xis 0 left 31 right 31]
+  (loop [xis 0]
     (when (< xis limit)
-      (loop [i 0 l left]
-        (when (<= i l)
-          (print "_")
-          (recur (inc i) l)))
       (loop [yis 0]
         (when (<= yis xis)
-          (if (even? (quot (factorial xis) (* (factorial yis) (factorial (- xis yis)))))
-            (print "_")
-            (print 1))
+          (print (quot (factorial xis) (* (factorial yis) (factorial (- xis yis)))))
+          (if (not= yis xis)
+            (print " ")
+            (println))
           (recur (inc yis))))
-      (loop [i 0 r right]
-        (when (<= i r)
-          (print "_")
-          (recur (inc i) r)))
-      (println)
-      (recur (inc xis) left (dec right)))))
+      (recur (inc xis)))))
 
 (triangle (read-string (read-line)))
